ddbc26608e7a8557ed1dddfc7cda54948e6cb5b3
PARTHG0106 <parthgoyal607@gmail.com>
2026-09-26 13:08:27 +0530

    Time: 63 ms (92.21%) | Memory: 122.7 MB (66.67%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    string evaluate(string s, auto& K) {
-        unordered_map<string, string> d;
-        for (auto& k : K)
-            d[k[0]] = k[1];
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> map;
+        for (auto& k : knowledge) map[k[0]] = k[1];
 
-        string res;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '(') {
-                int j = s.find(")", i + 1);
-                auto t = s.substr(i + 1, j - i - 1);
-                res += d.count(t) ? d[t] : "?";
-                i = j;
-            } else
-                res += s[i];
+        string ans, key;
+        bool inside = false;
+
+        for (char c : s) {
+            if (c == '(') { inside = true; key = ""; }
+            else if (c == ')') {
+                inside = false;
+                ans += map.count(key) ? map[key] : "?";
+            }
+            else if (inside) key += c;
+            else ans += c;
         }
-
-        return res;
+        return ans;
     }
 };
